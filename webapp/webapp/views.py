5145f912f0cfad73183a410db516fd67ff891dda
Alexander V <yoyo.aero@yandex.ru>
2017-10-21 10:42:34 +0300

    id hardcoded

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -16,15 +16,3 @@ def menu():
 @webapp.route('/games.html')
 def games():
     return render_template("games.html")
-
-@webapp.route('/csgo/stats')
-def csgo_stats():
-    return """<h1>This page is under construction</h1>
-    <img src="https://i.pinimg.com/736x/00/8e/8f/008e8f7c946120650b7b254e2a72e7a4--caution-signs-construction-\
-    signs.jpg" alt="Smiley face" height="500" width="500">"""
-
-@webapp.route('/csgo/team')
-def csgo_team():
-    return """<h1>This page is also under construction</h1>
-    <img src="https://i.pinimg.com/736x/00/8e/8f/008e8f7c946120650b7b254e2a72e7a4--caution-signs-construction-\
-    signs.jpg" alt="Smiley face" height="500" width="500">"""
