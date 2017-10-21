570d2548c91a0119a4919476d167faa46cf6dd3d
AndreyK2001 <K123a2013@yandex.ru>
2017-10-21 04:16:39 +0300

    just a commit

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -7,3 +7,14 @@ webapp.config.from_object("config")
 db = SQLAlchemy(webapp)
 
 from webapp import views, models
+
+#log in
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(webapp)
+lm.login_view = 'register'
+oid = OpenID(webapp, os.path.join(basedir, 'tmp'))
