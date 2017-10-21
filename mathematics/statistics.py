0233a258b68a87ce1fd99beacd5efc0cbdfc3053
AndreyK2001 <K123a2013@yandex.ru>
2017-10-22 01:53:01 +0300

    Add files via upload

--- mathematics/statistics.py
+++ mathematics/statistics.py
@@ -1,5 +1,4 @@
-
-from math import log10, log, e, sqrt
+from math import log, log10
 
 
 def E(R, *args):
@@ -39,16 +38,17 @@ def R_new(R, K, S, E):
 
 
 def set_R(*args):
-    args[2]=log10(args[2])*args[2]/3600
+    args[2]=log10(args[2]/3600)*args[2]/3600
     R=list(map(lambda a: a**2, args))
     #R=total_kills*log(total_time_played)*total_wins*total_matches_played*total_kills_headshot*total_mvps*total_shots_hit/(total_deaths*(total_deaths+total_kills)*total_shots_fired*total_shots_fired)
-    T=sum(R)*250/3
-    return T if T<=2500 else 2500
+    #T=sum(R)*250/3
+    Z=log10(sum(R))*100
+    return int(Z) if Z<=2500 else 2500
 
 
 def set_R(total_kills, total_deaths, total_time_played, total_wins, total_kills_headshot, total_shots_hit, total_shots_fired, total_mvps, total_matches_played):
-    total_time_played/=3600.0
+    total_time_played=log10(total_time_played/3600)*total_time_played/3600
     params=[total_kills, total_deaths, total_time_played, total_wins, total_kills_headshot, total_shots_hit, total_shots_fired, total_mvps, total_matches_played]
-    R=list(map(lambda a: a**2, params))
-    T=sum(R)*250/3
-    return T if T<=2500 else 2500
+    T=list(map(lambda a: a**2, params))
+    Z=log10(sum(T))*100
+    return int(Z) if Z<=2500 else 2500
